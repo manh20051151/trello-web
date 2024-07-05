4298a60cc99167226cda619b413a4d960e5ef2a1
manh20051151 <nguyenvietmanh1409@gmail.com>
2024-07-06 05:57:59 +0700

    congig_mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <div>manh</div>
+      <Typography variant='body2' color="text.secondary">hihih</Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
